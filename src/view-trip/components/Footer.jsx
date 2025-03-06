c01f279f79627a005705f549a0cb19c5d01709b2
Tushar <tushar.hasule@somaiya.edu>
2025-03-06 17:47:54 +0530

    minor_changes!

--- src/view-trip/components/Footer.jsx
+++ src/view-trip/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className='my-6'>
-      <h1 className='text-gray-900 font-bold text-center'>Created by Ankit     </h1>
+      <h1 className='text-gray-900 font-bold text-center'>Created by Tushar Hasule</h1>
       <h6 className='text-gray-400 text-center'> AI Trip Planner </h6>
     </div>
   )
